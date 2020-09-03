749195ca8982fae12899e6285dc269d4241e2e47
Moshweu Lesiba Johannes <v-moshweul@microsoft.com>
2020-09-03 16:55:00 +0200

    intial commit

---
+++ Data/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Data
+{
+    public class Item
+    {
+        public int id { get; set; }
+        public int itemCollectionId { get; set; }
+        public string name { get; set; }
+        public decimal price { get; set; }
+        public string description { get; set; }
+
+
+    }
+}
