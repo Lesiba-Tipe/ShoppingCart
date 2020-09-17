0963c6044542d0847c0c8df27abfd11ddd99bd30
Moshweu Lesiba Johannes <v-moshweul@microsoft.com>
2020-09-17 08:26:52 +0200

    If applied, this commit will change heading position, table style and body layout

--- Data/Item.cs
+++ Data/Item.cs
@@ -18,12 +18,13 @@ namespace ShoppingCart.Data
     {
         public List<Item> Items = new List<Item>()
 {
-            new Item(){ Id = 0, Name = "Laptop", Price = 7500, Description = "visit website..."},
-            new Item(){ Id = 1, Name = "Hard disk", Price = 1100, Description = "visit website..."},
-            new Item(){ Id = 2, Name = "office chair", Price = 600, Description = "visit website..."},
-            new Item(){ Id = 3, Name = "tablet", Price = 2100, Description = "visit website..."},
-            new Item(){ Id = 4, Name = "power bank", Price = 200, Description = "visit website..."},
-            new Item(){ Id = 5, Name = "back pack", Price = 500, Description = "visit website..."}
+            new Item(){ Id = 0, Name = "Laptop", Price = 9500, Description = "visit website..."},
+            new Item(){ Id = 1, Name = "Hard Disk", Price = 1100, Description = "visit website..."},
+            new Item(){ Id = 2, Name = "Office Chair", Price = 600, Description = "visit website..."},
+            new Item(){ Id = 3, Name = "Tablet", Price = 2100, Description = "visit website..."},
+            new Item(){ Id = 4, Name = "Power Bank", Price = 200, Description = "visit website..."},
+            new Item(){ Id = 5, Name = "Back Pack", Price = 500, Description = "visit website..."},
+           
         };
 
     }
